88937e8b3ad16b9135823d1489320ee9036ad43d
shironima <gabrielahenskysalaras@gmail.com>
2025-03-13 17:56:53 +0700

    modified:   resources/views/layouts/app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
 
     <!-- <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script> -->
 
-    <!-- Gunakan Tailwind dari Vite -->
+    <!-- Tailwind dari Vite -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     <!-- Bootstrap Icons -->
